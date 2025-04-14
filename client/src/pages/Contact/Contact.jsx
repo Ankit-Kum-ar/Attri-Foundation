45bdabfe7e94967caf038c350454ae35fbed31fe
Sahil Rawat <sahilr8587@gmail.com>
2025-04-14 12:16:42 +0530

    Add Map component to Contact page for enhanced location visibility

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import {ContactForm, Hero} from './components'
+import {ContactForm, Hero, Map} from './components'
+// import Map from './components/Map'
 const Contact = () => {
   return (
     <>
     <Hero />
     <ContactForm />
+    <Map />
     </>
   )
 }
--- client/src/pages/Contact/components/index.js
+++ client/src/pages/Contact/components/index.js
@@ -1,2 +1,3 @@
 export { default as Hero } from './Hero';
 export { default as ContactForm } from './ContactForm';
+export { default as Map } from './Map'
